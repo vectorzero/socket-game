b357ee34a0598f5cec496087f7fd73c237b74d38
vectorzero <w3298028@163.com>
2018-12-17 23:42:53 +0800

    web server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+// websocket
 var ws = require("nodejs-websocket");
 console.log("开始建立连接...")
-
 var $controller = null,$display = null , $controllerReady = false , $displayReady = false;
 var server = ws.createServer(function(conn){
     conn.on("text", function (str) {
@@ -26,4 +26,28 @@ var server = ws.createServer(function(conn){
         console.log("异常关闭")
     });
 }).listen(8111)
-console.log("WebSocket建立完毕")
+console.log("WebSocket建立完毕");
+
+// web server
+var c = require('child_process');
+var os = require('os');
+var express = require("express");
+var app = express();
+app.use(express.static("./")).listen(8000);
+
+var getIPAddress = function () {
+  var ifaces = os.networkInterfaces();
+  var ip = '';
+  for (var dev in ifaces) {
+    ifaces[dev].forEach(function (details) {
+      if (ip === '' && details.family === 'IPv4' && !details.internal) {
+        ip = details.address;
+        return;
+      }
+    });
+  }
+  return ip || "127.0.0.1";
+};
+let hostname = getIPAddress();
+let ipAdress = `http://${hostname}:8000`;
+c.exec(`start ${ipAdress}`);
