46b6998ab2e37b44ef32c1fd10d2c24cc06fea06
vectorzero <w3298028@163.com>
2018-12-17 23:52:09 +0800

    format

--- server.js
+++ server.js
@@ -3,28 +3,28 @@ var ws = require("nodejs-websocket");
 console.log("开始建立连接...")
 var $controller = null,$display = null , $controllerReady = false , $displayReady = false;
 var server = ws.createServer(function(conn){
-    conn.on("text", function (str) {
-        console.log("收到的信息为:"+str)
-        if(str === "😏") {
-            $controller = conn;
-            $controllerReady = true;
-            conn.sendText("success");
-        }
-        if(str === "display") {
-            $display = conn;
-            $displayReady = true;
-        }
-        if($controllerReady && $displayReady){
-            $display.sendText(str);
-        }
-        conn.sendText("😃")
-    })
-    conn.on("close", function (code, reason) {
-        console.log("关闭连接")
-    });
-    conn.on("error", function (code, reason) {
-        console.log("异常关闭")
-    });
+  conn.on("text", function (str) {
+    console.log("收到的信息为:"+str)
+    if(str === "😏") {
+      $controller = conn;
+      $controllerReady = true;
+      conn.sendText("success");
+    }
+    if(str === "display") {
+      $display = conn;
+      $displayReady = true;
+    }
+    if($controllerReady && $displayReady){
+      $display.sendText(str);
+    }
+    conn.sendText("😃")
+  })
+  conn.on("close", function (code, reason) {
+    console.log("关闭连接")
+  });
+  conn.on("error", function (code, reason) {
+    console.log("异常关闭")
+  });
 }).listen(8111)
 console.log("WebSocket建立完毕");
 
